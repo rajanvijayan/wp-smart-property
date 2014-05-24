5f5c164e012d2b6b0e04379f857db086150713c8
rajanit2000 <rajanit2000@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-24 12:33:11 +0000

    Update Version 1.0 to 2.0
    git-svn-id: https://plugins.svn.wordpress.org/wp-smart-property/trunk@920339 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- install.php
+++ install.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: WP Smart Property
 Author: <strong>Rajan V</strong>
-Version: 1.0
+Version: 2.0
 Description: This is realestate plugin. It have a widget you can use it very simple steps.
 */
 
