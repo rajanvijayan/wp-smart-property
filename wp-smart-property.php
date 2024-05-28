8a2fc8960a0c17d5d0cfbbffebcda8e10a1e7d29
Rajan Vijayan <rajanit2000@gmail.com>
2024-05-29 00:04:32 +0530

    update version

--- wp-smart-property.php
+++ wp-smart-property.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WP Smart Property
  * Description: WP Smart Property is a comprehensive WordPress plugin designed to manage property listings with ease. This plugin allows users to create, edit, and display property listings on their WordPress website. It provides an intuitive interface for managing property-related taxonomies and meta information, making it perfect for real estate agencies, property managers, and individual property owners.
- * Version: 1.0
+ * Version: 3.0.0
  * Author: Rajan Vijayan
  * Author URI: https://rajanvijayan.com
  * Text Domain: wp-smart-property
